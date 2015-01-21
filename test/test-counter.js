ff981e386992c59a28d4495b182bb8503e088820
Jacqueline <Jacqueline@Jacquelines-MacBook-Pro.local>
2015-01-21 11:32:13 -1000

    Test written for count function.

--- test/test-counter.js
+++ test/test-counter.js
@@ -1,1 +1,17 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
 
+var mda = require('../MultiDimensionalArray.js');
+
+describe("MultiDimensionalArray", function() {
+  describe("count", function() {
+    it("one-arg (array) case: should return a number (how many truthy values there are inside array (regardless of depth)", function() {
+      var sample_array = [1,0,0,1,0,0,0,0,1];
+      var result = mda.count(sample_array);
+
+      expect(result).to.equal(3);
+    });
+  });
+  
+});

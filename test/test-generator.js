cdac7150b99d673bbfccbc1451ffdcda13fb9196
Jacqueline <Jacqueline@Jacquelines-MacBook-Pro.local>
2015-01-20 19:55:15 -1000

    Oops. Earlier commit was just for calling generate() with no arguments. This time test-generator tests calling generate() with a single argument.

--- test/test-generator.js
+++ test/test-generator.js
@@ -11,15 +11,25 @@ describe("MultiDimensionalArray", function() {
       expect(mda.generate).to.be.instanceof(Function);
     });
   
+    describe("calling with no arguments", function() {
+      it('should return an array (length 3) each containing arrays (length 3) with random boolean values', function() {
+        var result = mda.generate();
 
-    it('should return an array (length 3) each containing arrays (length 3) with random boolean values', function() {
-      var result = mda.generate();
+        result.should.be.an('array');
+        result.should.have.length(3);
+        result[0].should.have.length(3);
+        result[1].should.have.length(3);
+        result[2].should.have.length(3);
+      });
+    });
 
-      result.should.be.an('array');
-      result.should.have.length(3);
-      result[0].should.have.length(3);
-      result[1].should.have.length(3);
-      result[2].should.have.length(3);
+    describe("calling with one argument", function() {
+      it('should return single array (length 4)', function () {
+        var result = mda.generate(4);
+
+        result.should.be.an('array');
+        result.should.have.length(4);
+      });
     });
 
   }); // desc('Generate Function')

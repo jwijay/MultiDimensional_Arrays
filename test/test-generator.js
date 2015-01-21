6a46c38331de067727b559e4e4e407493a875874
Jacqueline <Jacqueline@Jacquelines-MacBook-Pro.local>
2015-01-21 11:10:24 -1000

    generate_3d() tests written

--- test/test-generator.js
+++ test/test-generator.js
@@ -14,6 +14,7 @@ describe('MultiDimensionalArray', function() {
     it('empty args case: should return array (length 3) with random bool values', function() {
       var result = mda.generate_1d();
 
+      expect(result).to.be.instanceof(Array);
       result.should.have.length(3);
       result.forEach(function(value) {
         expect(value).to.be.a('boolean');
@@ -23,6 +24,7 @@ describe('MultiDimensionalArray', function() {
     it('one arg case: should return single array (length 4) with random bool values', function() {
       var result = mda.generate_1d(4);
 
+      expect(result).to.be.instanceof(Array);
       result.should.have.length(4);
       //Do I have to repeat this code from the empty args case?? O___o
       result.forEach(function(value) {
@@ -48,6 +50,7 @@ describe('MultiDimensionalArray', function() {
     it("empty args case: should return array (length 3) of arrays (each length 3) containing random bool values", function() {
       var result = mda.generate_2d();
 
+      expect(result).to.be.instanceof(Array);
       expect(result).should.have.length(3);
 
       result.forEach(function(array) {
@@ -63,6 +66,7 @@ describe('MultiDimensionalArray', function() {
     it("one arg case: should return array (length 4) of arrays (each length 3) containing random bool values", function() {
       var result = mda.generate_2d(4);
 
+      expect(result).to.be.instanceof(Array);
       expect(result).should.have.length(4);
 
       result.forEach(function(array) {
@@ -78,6 +82,7 @@ describe('MultiDimensionalArray', function() {
     it("two args case: should return array (length 5) of arrays (each length 4) containing random bool values", function() {
       var result = mda.generate_2d(5,4);
 
+      expect(result).to.be.instanceof(Array);
       expect(result).should.have.length(5);
 
       result.forEach(function(array) {
@@ -90,8 +95,37 @@ describe('MultiDimensionalArray', function() {
       });
     });
 
-
-
   }); //desc generate_2d
+
+
+  describe("generate_3d", function() {
+    it('should be defined as a function', function() {
+      expect(mda.generate_3d).to.be.a('function');
+      expect(mda.generate_3d).to.be.instanceof(Function);
+    });
+
+    it("three args case: should return array (length 3) of arrays (each length 4) containing arrays (length 5) of random bool values", function() {
+      var result = mda.generate_3d(3,4,5);
+
+      expect(result).to.be.instanceof(Array);
+      expect(result).should.have.length(3);
+
+      result.forEach(function(second_dimension) {
+        expect(second_dimension).to.be.instanceof(Array);
+        second_dimension.should.have.length(4);
+
+        second_dimension.forEach(function(first_dimension) {
+
+          first_dimension.should.have.length(5);
+
+          first_dimension.forEach(function(value) {
+            expect(value).to.be.a('boolean');
+          });
+
+       });
+      });
+    });
+
+  }); //desc generate_3d
 
 }); //desc MultiDim(blahblah)
